d5f9eee1237231bfb4f981298b6b4a0b338e5ff5
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2020-12-22 16:13:50 -0500

    added validation and delete request

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -4,6 +4,21 @@ const db = require('../data/connect')
 const router = express.Router()
 router.use(express.json()) // add midleware json to read json formst from req.body
 
+
+// check if id exists
+
+const validateId =  ()=>{
+    return  async (req,res,next)=>{
+       let id = await db('cars').where("id",req.params.id).first()
+       console.log(id)
+       if(!id){
+           return res.status(404).json({message:`No Car exists with id ${req.params.id}`})
+       }
+       next()
+  
+    }
+}
+
 //get all cars
 router.get('/cars', async (req,res,next)=>{
     try{
@@ -14,7 +29,7 @@ router.get('/cars', async (req,res,next)=>{
 
 // get one car based on id
 
-router.get('/cars/:id', async (req,res,next)=>{
+router.get('/cars/:id', validateId(), async (req,res,next)=>{
     try{
         const id = req.params.id
         console.log('id is',{id})
@@ -35,7 +50,10 @@ router.post('/cars', async (req,res,next)=>{
             model: req.body.model,
             mileage: req.body.mileage
         }
-        const [id] = await db('cars').insert(req.body) // insert newcar and return the ids
+        if(!payload.vin || !payload.make || !payload.model || !payload.mileage){
+            return res.status(400).json({message:'missing vin or make or model or mileage'})
+        }
+        const [id] = await db('cars').insert(payload) // insert newcar and return the ids it works either as payload or just req.body
         const newCar = await db('cars').where({id}).first() // return added car based on ids[0]]
         res.status(201).json(newCar)
 
@@ -44,5 +62,44 @@ router.post('/cars', async (req,res,next)=>{
         next(err)
     }
 })
+
+//  edit car information
+
+router.put('/cars/:id', validateId(), async (req,res,next)=>{
+
+    try{
+        let payload = {
+            vin: req.body.vin,
+            make: req.body.make,
+            model: req.body.model,
+            mileage: req.body.mileage,
+            type: req.body.type,
+            status: req.body.status
+        }
+        if(!payload.vin || !payload.make || !payload.model || !payload.mileage){
+            return res.status(400).json({message:'missing vin, make, model, or mileage'})
+        }
+        await db('cars').where("id",req.params.id).update(payload)
+        const updated  = await db('cars').where('id',req.params.id).first() 
+         res.send(updated)
+        
+   
+
+    }
+    catch(err){next(err)}
+})
+
+
+
+// delete a car based on an id
+router.delete('/cars/:id',validateId(), async (req,res,next)=>{
+
+    try{
+        await db('cars').where("id",req.params.id).del()
+        res.send({message:`car with id ${req.params.id} is deleted successfully`})
+    }
+    catch(err){next(err)}
+}) 
+
 
 module.exports = router
